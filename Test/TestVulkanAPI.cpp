395154f153564065ad21d44991621942fc866aa0
杨东（yangdong01）-基础架构部 <yangdong01@qiyi.com>
2021-05-12 16:03:31 +0800

    add vulkan tutorial code

--- Test/TestVulkanAPI.cpp
+++ Test/TestVulkanAPI.cpp
@@ -1,13 +1,111 @@
-#include "RHI/VulkanRHI.h"
+//#include "RHI/VulkanRHI.h"
 
-using namespace MetaInit;
+#include "gtest/gtest.h"
+#include <vector>
+#include <optional>
+
+//using namespace MetaInit;
 
 TEST(TEST_RHI_MODULE, TEST_VULKAN_INIT)
 {
-	__noop();
+	EXPECT_TRUE(true);
 }
 
 TEST(TEST_RHI_MODULE, TEST_VULKAN_PIPLINE)
 {
+	EXPECT_TRUE(true);
+}
 
+#define GLFW_INCLUDE_VULKAN
+#include "glfw3.h"
+
+TEST(TEST_RHI_MODULE, TEST_API_TUTORIAL)
+{
+	uint32_t extension_count = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+	EXPECT_GE(extension_count, 1) << "NV GPU should support more than 1 extensions";
+	std::vector<VkExtensionProperties> extension_props(extension_count);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, extension_props.data());	
+	for (const auto& prop : extension_props)
+	{
+		std::cout << prop.extensionName << ":" << prop.specVersion << "\n";
+	}
+	std::cout << std::endl;
+	VkInstanceCreateInfo instance_info;
+	instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_info.pNext = nullptr;
+	instance_info.pApplicationInfo = nullptr;
+	instance_info.enabledLayerCount = 0;
+	VkInstance instance;
+	VkResult result = vkCreateInstance(&instance_info, nullptr, &instance);
+	ASSERT_EQ(result, VK_SUCCESS);
+	EXPECT_TRUE(instance != nullptr);
+
+	//query device
+	uint32_t device_count = 0;
+	vkEnumeratePhysicalDevices(instance, &device_count, nullptr);
+	EXPECT_LE(device_count, 1) << "should have at least one device";
+	std::vector<VkPhysicalDevice> phy_devices(device_count);
+	vkEnumeratePhysicalDevices(instance, &device_count, phy_devices.data());
+	for (const auto& phd : phy_devices)
+	{
+		VkPhysicalDeviceProperties device_props;
+		vkGetPhysicalDeviceProperties(phd, &device_props);
+		std::cout << std::uppercase << device_props.deviceName << std::endl;
+		std::cout << device_props.deviceType << std::endl;
+	}
+	std::cout << std::endl;
+	//create logic device
+	VkPhysicalDeviceFeatures device_feature{};
+	vkGetPhysicalDeviceFeatures(phy_devices[0], &device_feature);
+	ASSERT_TRUE(device_feature.geometryShader);
+	VkDeviceCreateInfo device_info{};
+	device_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_info.enabledExtensionCount = 0; //FIXME
+	device_info.enabledLayerCount = 0;
+	device_info.pEnabledFeatures = &device_feature;
+
+	//query device queue
+	std::optional<uint32_t> family_indice;
+	auto queue_family = [&](const uint32_t queue_flags)
+	{
+		uint32_t family_count = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(phy_devices[0], &family_count, nullptr);
+		std::vector<VkQueueFamilyProperties> familys_props(family_count);
+		vkGetPhysicalDeviceQueueFamilyProperties(phy_devices[0], &family_count, familys_props.data());
+		
+		uint32_t index = 0;
+		for (const auto& family : familys_props)
+		{
+			std::cout << family.queueFlags << std::endl;
+			if (family.queueFlags & queue_flags)
+			{
+				family_indice = index;
+				break;
+			}
+			++index;
+		}
+	};
+	
+	queue_family(VK_QUEUE_COMPUTE_BIT);
+	ASSERT_TRUE(family_indice.has_value());
+	VkDeviceQueueCreateInfo queue_info{};
+	queue_info.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_info.queueFamilyIndex = family_indice.value();
+	constexpr float queue_prior = 1.0f;
+	queue_info.pQueuePriorities = &queue_prior;
+	queue_info.pNext = nullptr;
+
+	device_info.pQueueCreateInfos = &queue_info;
+	device_info.queueCreateInfoCount = 1;
+	VkDevice device{};
+	result = vkCreateDevice(phy_devices[0], &device_info, nullptr, &device);
+	ASSERT_TRUE(result == VK_SUCCESS) << __LINE__ << "create device failed";
+	VkQueue graph_queue;
+	vkGetDeviceQueue(device, family_indice.value(), 0, &graph_queue);
+	ASSERT_TRUE(graph_queue != VK_NULL_HANDLE);
+
+	//gtest other things
+	vkDestroyDevice(device, nullptr);
+	vkDestroyInstance(instance, nullptr);
 }
