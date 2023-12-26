e87f9ea55357670f38e2942b856c97aa8cbb6cee
Buryang <419494197@163.com>
2023-12-26 14:41:50 +0800

    Memory.cpp
    refactor code to not use bitmask to flag used blocks

--- Utils/Memory.h
+++ Utils/Memory.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Utils/CommonUtils.h"
-#include "folly/memory/arena.h"
+//http://dmitrysoshnikov.com/compilers/writing-a-memory-allocator/
 
 namespace Shard
 {
@@ -10,10 +10,10 @@ namespace Shard
 		{
 		public:
 			explicit LinearAllocatorImpl(size_type capacity);
-			void* Alloc(size_type size, size_type n);
-			void DeAlloc(void* ptr, size_type n);
+			void* allocate(size_type size, size_type n);
+			void deallocate(void* ptr, size_type n);
 			void Reset();
-			const size_type GetCapacity()const {
+			const size_type max_size()const {
 				return capacity_;
 			}
 			~LinearAllocatorImpl();
@@ -30,25 +30,21 @@ namespace Shard
 			//field for allocator_traits
 			using value_type = T;
 			using pointer = T*;
-			using allocate = Alloc;
-			using deallocate = DeAlloc;
-			using max_size = GetCapacity;
 			template<class U>
 			struct rebind {
 				using other = LinearAllocator<U>;
 			};
 			template<class>
 			friend class LinearAllocator;
-
 		public:
-			explicit LinearAllocator(size_type capacity = 1024 * 1024) :impl_(std::make_shared<LinearAllocatorImpl>(capacity) {
+			explicit LinearAllocator(size_type capacity = 1024 * 1024) :impl_(std::make_shared<LinearAllocatorImpl>(capacity)){
 			}
 			template<class U>
 			LinearAllocator(const LinearAllocator<U>& other) noexcept : impl_(other.impl_) //todo
 			{
 
 			}
-			[[nodiscard]] pointer Alloc(size_type n = 1u) {
+			[[nodiscard]] pointer allocate(size_type n = 1u) {
 				pointer ptr = impl_->Alloc(sizeof(T), n);
 				if (nullptr != ptr) {
 					return reinterpret_cast<T*>(ptr);
@@ -57,13 +53,13 @@ namespace Shard
 			}
 			template <typename... Args>
 			[[nodiscard]] pointer AllocNoDestruct(Args&&... args) {
-				auto ptr = Alloc();
+				auto ptr = allocate();
 				return new(ptr)T(std::forward<Args>(args)...);
 			}
-			void DeAlloc(pointer ptr, size_type n) {
+			void deallocate(pointer ptr, size_type n) {
 				impl_->DeAlloc(ptr, n * sizeof(T));
 			}
-			size_type GetCapacity()const {
+			size_type max_size()const {
 				return impl_->GetCapacity() / sizeof(value_type);
 			}
 			~LinearAllocator() = default;
@@ -87,8 +83,8 @@ namespace Shard
 		public:
 			StackAllocatorImpl() = default;
 			explicit StackAllocatorImpl(size_type capacity);
-			void* Alloc(size_type size, size_type n);
-			void DeAlloc(void* ptr, size_type n);
+			void* allocate(size_type size, size_type n);
+			void deallocate(void* ptr, size_type n);
 			void Reset();
 			~StackAllocatorImpl();
 		private:
@@ -103,8 +99,6 @@ namespace Shard
 		public:
 			//field for allocator_traits
 			using value_type = T;
-			using allocate = Alloc;
-			using deallocate = DeAlloc;
 			template<class U>
 			struct rebind
 			{
@@ -121,15 +115,15 @@ namespace Shard
 			StackAllocator(const StackAllocator<U>& other) noexcept :impl_(other.impl_)
 			{
 			}
-			[[nodiscard]] T* Alloc(size_type n = 1u) {
+			[[nodiscard]] T* allocate(size_type n = 1u) {
 				return reinterpret_cast<T*>(impl_->Alloc(sizeof(T), n));
 			}
 			template <typename... Args>
 			[[nodiscard]] T* AllocNoDestruct(Args&&... args) {
-				auto ptr = Alloc();
+				auto ptr = allocate();
 				return new(ptr)T(std::forward<Args>(args)...);
 			}
-			void DeAlloc(T* ptr, size_type n) {
+			void deallocate(T* ptr, size_type n) {
 				std::destroy(ptr, ptr + n);
 				impl_->DeAlloc(ptr, sizeof(T) * n);
 			}
@@ -165,9 +159,9 @@ namespace Shard
 				Init(element_count);
 			}
 			void Init(size_type element_count) {
-				memory_ = ;; operator new(sizeof(Node) * element_count);
-				head£ß = reinterpret_cast<Node*>(memory_);
-				auto* tail = head_;
+				memory_ = ::operator new(sizeof(Node) * element_count);
+				free_head_ = reinterpret_cast<Node*>(memory_);
+				auto* tail = free_head_;
 				//constructor free list
 				for (auto n = 0; n < element_count; ++n, ++tail) {
 					tail->next_ = tail + 1;
@@ -175,7 +169,7 @@ namespace Shard
 				tail->next_ = nullptr;
 			}
 			//todo lock
-			[[nodiscard]] T* Alloc() {
+			[[nodiscard]] T* allocate() {
 				auto* next = free_head_->next_;
 				if (next == nullptr) {
 					return nullptr;
@@ -185,10 +179,10 @@ namespace Shard
 			}
 			template <typename... Args>
 			[[nodiscard]] T* AllocNoDestruct(Args&&... args) {
-				auto ptr = Alloc();
+				auto ptr = allocate();
 				return new(ptr)T(std::forward<Args>(args)...);
 			}
-			void DeAlloc(T* ptr) {
+			void deallocate(T* ptr) {
 				std::destroy(ptr, ptr + 1);//to do
 				auto* node = reintepret_cast<Node*>(static_cast<uintptr_t>(ptr) - sizeof(Node*));
 				node->next_ = free_head_;
@@ -198,8 +192,8 @@ namespace Shard
 			friend bool operator==(const PooledAllocator& lhs, const PooledAllocator& rhs) {
 				return rhs.memory_ == rhs.memory_;
 			}
-			friend bool operator!=(const PooledAllocator& rhs, const PooledAllocator& rhs) {
-				return !(rhs == rhs);
+			friend bool operator!=(const PooledAllocator& lhs, const PooledAllocator& rhs) {
+				return !(lhs == rhs);
 			}
 		private:
 			DISALLOW_COPY_AND_ASSIGN(PooledAllocator);
@@ -219,40 +213,61 @@ namespace Shard
 			struct PoolBucketInfo
 			{
 				using Type = std::tuple<>;
+				static constexpr size_type MaxSize = 0u;
 			};
 
+			//todo size from https://www.bsdcan.org/2006/papers/jemalloc.pdf
 			struct BucketType16
 			{
 				static constexpr auto BLK_SIZE = 16u;
-				static constexpr auto BLK_COUNT = 10000u;
+				static constexpr auto BLK_COUNT = 1000u;
 			};
 
 			struct BucketType32
 			{
 				static constexpr auto BLK_SIZE = 32u;
-				static constexpr auto BLK_COUNT = 10000u;
+				static constexpr auto BLK_COUNT = 1000u;
+			};
+
+			struct BucketType64
+			{
+				static constexpr auto BLK_SIZE = 64u;
+				static constexpr auto BLK_COUNT = 1000u;
 			};
 
 			struct BucketType128
 			{
 				static constexpr auto BLK_SIZE = 128u;
-				static constexpr auto BLK_COUNT = 10000u;
+				static constexpr auto BLK_COUNT = 100u;
+			};
+
+			struct BucketType256
+			{
+				static constexpr auto BLK_SIZE = 256u;
+				static constexpr auto BLK_COUNT = 512u;
+			};
+
+			struct BucketType512
+			{
+				static constexpr auto BLK_SIZE = 128u;
+				static constexpr auto BLK_COUNT = 100u;
 			};
 
 			struct BucketType1024
 			{
 				static constexpr auto BLK_SIZE = 1024u;
-				static constexpr auto BLK_COUNT = 10000u;
+				static constexpr auto BLK_COUNT = 10u;
 			};
-#define REGIST_POOL_ID(ID) template<> struct PoolBucketInfo<ID> { using Type = std::tuple<BucketType16, BucketType32, BucketType128, BucketType1024>; };
+#define REGIST_POOL_ID(ID) template<> struct PoolBucketInfo<ID> { using Type = std::tuple<BucketType16, BucketType32, BucketType64, BucketType128, BucketType256, BucketType512, BucketType1024>;  static constexpr size_type MaxSize = BucketType1024::BLK_SIZE; };
 
+			//todo defrag ? like https://github.com/jemalloc/jemalloc/issues/566 ??
 			class Bucket final
 			{
 			public:
 				explicit Bucket(size_type blk_size, size_type blk_count);
 				~Bucket();
-				void* Alloc(size_type size);
-				void DeAlloc(void* ptr, size_type size);
+				void* allocate(size_type size);
+				void deallocate(void* ptr, size_type size);
 				bool IsPointerBelong(void* ptr)const;
 				FORCE_INLINE size_type GetBlkSize()const {
 					return blk_size_;
@@ -271,26 +286,27 @@ namespace Shard
 				void* ledger_{ nullptr };
 			};
 
+
 			template<size_type id>
 			using PoolBucketInfo_Type = PoolBucketInfo<id>::Type;
 			template<size_type id>
-			struct PoolBucketInfo_TypeCount : std::integral_constant < size_type, std::tuple_size_v<PoolBucketInfo_Type<id> > {};
+			struct PoolBucketInfo_TypeCount : std::integral_constant<size_type, std::tuple_size_v<PoolBucketInfo_Type<id>>>{};
 			template<size_type id, size_type index>
 			struct BucketBlkSize : std::integral_constant<size_type, std::tuple_element_t<index, PoolBucketInfo_Type<id>>::BLK_SIZE> {};
 			template<size_type id, size_type index>
 			struct BucketBlkCount : std::integral_constant<size_type, std::tuple_element_t<index, PoolBucketInfo_Type<id>>::BLK_COUNT> {};
 			template<size_type id>
-			struct PoolType : SmallVector<Bucket, PoolBucketInfo_TypeCount<id>, false>;
+			struct PoolType : SmallVector<Bucket, PoolBucketInfo_TypeCount<id>::value, false> {};
 
 			template<size_type id, size_type ...index>
-			auto& PoolInstance(std::index_sequence<index...>) {
-				static PoolType<id> instance{ { { BucketBlkSize<id, index>::value, BucketBlkCount<id, index>::value }...} };
+			auto& PoolInstance(std::index_sequence<index...>) noexcept{
+				static PoolType<id> instance{ { { BucketBlkSize<id, index>::value, BucketBlkCount<id, index>::value } ...} };
 				return instance;
 			}
 
 			template<size_type id>
-			auto& PoolInstance() {
-				return PoolInstance<id>(std::make_index_sequence<PoolBucketInfo_TypeCount<id> >());
+			auto& PoolInstance() noexcept {
+				return PoolInstance<id>(std::make_index_sequence<PoolBucketInfo_TypeCount<id>::value>());
 			}
 
 			namespace {
@@ -304,100 +320,238 @@ namespace Shard
 				}
 			}
 			template<size_type id>
-			[[nodiscard]] void* Alloc(size_type size) {
-				auto& pool = PoolInstance<id>();
-				SmallVetor<Info, PoolBucketInfo_TypeCount<id>, false> delta;
-				size_type index{ 0u };
-				for (auto& bucket : pool) {
-					if (auto blk_size = bucket.GetBlkSize(); blk_size > size) {
-						delta.emplace_back({ index, 1u, blk_size - size });
+			[[nodiscard]] void* allocate(size_type size) {
+				if LIKELY(size <= PoolBucketInfo_Type<id>::MaxSize)
+				{
+					auto& pool = PoolInstance<id>();
+					SmallVector<Info, PoolBucketInfo_TypeCount<id>, false> delta;
+					size_type index{ 0u };
+					for (auto& bucket : pool) {
+						if (auto blk_size = bucket.GetBlkSize(); blk_size > size) {
+							delta.emplace_back({ index, 1u, blk_size - size });
+						}
+						else {
+							const auto blk_count = 1u + (size - 1u) / bucket.GetBlkSize();
+							delta.emplace_back({ index, blk_count, blk_count * bucket.GetBlkSize() - size });
+						}
+						++index;
 					}
-					else {
-						const auto blk_count = 1 + (size - 1) / bucket.GetBlkSize();
-						delta.emplace_back({ index, blk_count, blk_count * bucket.GetBlkSize() - size });
+					eastl::heap_sort(delta.begin(), delta.end());
+					for (const auto& dt : delta) {
+						if (auto ptr = pool[dt.index_].Alloc(size); ptr != nullptr) {
+							return ptr;
+						}
 					}
-					++index;
+					return nullptr;
 				}
-				eastl::sort(delta.begin(), delta.end());
-				for (const auto& dt : delta) {
-					if (auto ptr = pool[dt.index_].Alloc(size); ptr != nullptr) {
-						return ptr;
-					}
+				else
+				{
+					return ::operator new(size); //alloc big chunk
 				}
-				return nullptr;
 			}
 
 			template<size_type id>
-			void DeAlloc(void* ptr, size_type size) {
-				auto& pool = PoolInstance<id>();
-				for (auto& bucket : pool) {
-					if (bucket.IsPointerBelong(ptr)) {
-						bucket.DeAlloc(ptr, size);
-						break;
+			void deallocate(void* ptr, size_type size) {
+				if LIKELY(size <= PoolBucketInfo_Type<id>::MaxSize) {
+					auto& pool = PoolInstance<id>();
+					for (auto& bucket : pool) {
+						if (bucket.IsPointerBelong(ptr)) {
+							bucket.deallocate(ptr, size);
+							break;
+						}
 					}
 				}
+				else
+				{
+					::operator delete(ptr); //free big chunk
+				}
+			}
+
+			namespace Scalable
+			{
+				enum {
+					SCALABLE_BUCKET_FAKE_ID = 1024,
+					SCALEBLE_CHUNK_SIZE = 2 * 1024 * 1024,
+				};
+
+				struct FreeBlock
+				{
+					FreeBlock* next_{ nullptr };
+				};
+
+				struct Chunk
+				{
+					uint32_t free_blks_{ 0u };
+					Chunk* prev_{ nullptr };
+					Chunk* next_{ nullptr };
+					void* ledger_{ nullptr };
+					void* memory_{ nullptr };
+					FreeBlock* free_list_{ nullptr };
+				};
+
+
+				class ScalableBucket final
+				{
+				public:
+					friend class ScalablePool;
+					ScalableBucket(size_type blk_size);
+					[[nodiscard]] void* allocate([[maybe_unused]] size_type size);
+					//default deallocate; do deallcating in this thread
+					void deallocate(void* ptr, [[maybe_unused]] size_type size);
+					//do deallocating in another thread
+					void deallocate_external(void* ptr, [[maybe_unused]] size_type size);
+					~ScalableBucket();
+				private:
+					Chunk* AllocChunk();
+					void DeAllocChunk(Chunk* chunk);
+					void RecyleChunks();
+					bool TryRecyleExternalFrees();
+				private:
+					Chunk*	active_chunk_{ nullptr };
+					const size_type	blk_size_{ 0u };
+					const size_type	blk_count_{ 0u };
+					//std::atomic_bool	lock_; //nouse in a single thread, but now maybe alloc in one thread and release in another
+					std::atomic<FreeBlock*>	external_freed_;
+				};
+
+				class ScalablePool
+				{
+				public:
+					ScalablePool() = default;
+					[[nodiscard]] void* allocate(size_type size);
+					void deallocate(void* ptr, size_type size);
+					void deallocate_external(void* ptr, size_type size);
+				private:
+					Array<ScalableBucket, PoolBucketInfo_TypeCount<SCALABLE_BUCKET_FAKE_ID>::value>	buckets_;
+				};
 			}
 
 		}
 
-		//one stateless allocator
-		template<typename T, size_type id = 0>
+		//one stateless allocator. static prealloc pool
+		template<typename T, size_type id = 0u>
 		class StaticPoolAllocator
 		{
 		public:
 			using value_type = T;
-			using allocate = Alloc;
-			using deallocate = DeAlloc;
-			template<class U>
+			template<typename U>
 			struct rebind {
 				using other = StaticPoolAllocator<U, id>;
 			};
 		public:
 			StaticPoolAllocator() = default;
 			template<typename U>
-			StaticPoolAlloactor(const StaticPoolAllocator<U, id>& other) {
+			StaticPoolAllocator(const StaticPoolAllocator<U, id>& other) {
 
 			}
-			[[nodiscard]] T* Alloc(size_type n) {
-				return static_cast<T*>(MemoryPool::Alloc<id>(sizeof(T) * n);
+			[[nodiscard]] T* allocate(size_type n) {
+				return static_cast<T*>(MemoryPool::allocate<id>(sizeof(T) * n));
 			}
-			void DeAlloc(T* ptr, size_type n) {
-				MemoryPool::DeAlloc<id>(ptr, sizeof(T) * n);
+			void deallocate(T* ptr, size_type n) {
+				MemoryPool::deallocate<id>(ptr, sizeof(T) * n);
 			}
 			template<class U>
-			friend bool operator==(const StaticPoolAllocator& lhs, const StaticPoolAllocator<U>& rhs) {
+			friend bool operator==(const StaticPoolAllocator& lhs, const StaticPoolAllocator<U, id>& rhs) {
 				return true;
 			}
 			template<class U>
-			friend bool operator!=(const StaticPoolAllocator& lhs, const StaticPoolAllocator<U>& rhs) {
+			friend bool operator!=(const StaticPoolAllocator& lhs, const StaticPoolAllocator<U, id>& rhs) {
 				return false;
 			}
 		};
+
+		enum
+		{
+			POOL_DEFAULT_ID		= 0x0u,
+#ifdef USE_UNIFORM_POOL
+			POOL_JOBSYSTEM_ID = POOL_DEFAULT_ID,
+			POOL_RHI_ID = POOL_DEFAULT_ID,
+#else
+			// GUID first 64bit for static pool allocator ID
+			POOL_JOBSYSTEM_ID	= 0x09CD85C7477D493E,
+			POOL_RHI_ID			= 0xA14FB580D83043EE,
+#endif
+		};
+
+		//one stateful scalable dynamic pool allocator
+		template<typename T>
+		class ScalablePoolAllocator
+		{
+		public:
+			using value_type = T;
+			template<typename U>
+			struct rebind {
+				using other = ScalablePoolAllocator<U>;
+			};
+		public:
+			ScalablePoolAllocator() {
+				memory_pool_ = new Scalable::ScalablePool;
+			}
+			template<typename U>
+			ScalablePoolAllocator(const ScalablePoolAllocator<U>& other) :memory_pool_(other.memory_pool_) {}
+			template<typename U>
+			ScalablePoolAllocator(ScalablePoolAllocator<U>&& other) : memory_pool_(std::exchange(other.memory_pool_, nullptr)) {}
+
+			virtual ~ScalablePoolAllocato() {
+				delete std::exchange(memory_pool_, nullptr);
+			}
+			[[nodiscard]] T* allocate(size_type n) {
+				return static_cast<T*>(memory_pool_->allocate(sizeof(T) * n));
+			}
+			void deallocate(T* ptr, size_type n) {
+				if (owner_id_ == std::this_thread::get_id()) {
+					memory_pool_->deallocate(ptr, sizeof(T) * n);
+				}
+				else
+				{
+					//consumer in another thread
+					memory_pool_->deallocate_external(ptr, sizeof(T) * n);
+				}
+			}
+			template<class U>
+			friend bool operator==(const ScalablePoolAllocator& lhs, const ScalablePoolAllocator<U>& rhs) {
+				return lhs.memory_pool_ == rhs.memory_pool_;
+			}
+			template<class U>
+			friend bool operator!=(const ScalablePoolAllocator& lhs, const ScalablePoolAllocator<U>& rhs) {
+				return !(lhs == rhs);
+			}
+		private:
+			Scalable::ScalablePool* memory_pool_{ nullptr };
+			const std::thread::id	owner_id_{ std::this_thread::get_id() };
+		};
+
+		//scalable pool allocator for every thread
+		template<typename T, size_type id = 0u>
+		requires(!std::is_void_v<T>)
+		static decltype(auto) TLSScalablePoolAllocatorInstance()
+		{
+			thread_local ScalablePoolAllocator<T> allocator;
+			return &allocator;
+		}
 
 		template<typename T, template <typename> typename Allocator>
 		class TraceProxyAllocator 
 		{
 		public:
 			using value_type = T;
-			using allocate = Alloc;
-			using deallocate = DeAlloc;
-			template<class U>
+			template<typename U>
 			struct rebind {
-				using other = TraceProxyAllocator<U, Allocator<U>>;
+				using other = TraceProxyAllocator<U, Allocator>;
 			};
 		public:
-			TraceProxyAllocator(Allocator& alloc) :alloc_(alloc) {
+			TraceProxyAllocator(Allocator<T>& alloc) :alloc_(alloc) {
 
 			}
-			[[nodiscard]] T* Alloc(size_type n) {
+			[[nodiscard]] T* allocate(size_type n) {
 				++alloc_mem_count_;
 				alloc_mem_size_ += sizeof(T) * n;
-				return alloc_.Alloc(n);
+				return alloc_.allocate(n);
 			} 
-			void DeAlloc(T* ptr, size_type n) {
+			void deallocate(T* ptr, size_type n) {
 				--alloc_mem_count_;
 				alloc_mem_size_ -= sizeof(T) * n;
-				alloc_.DeAlloc(ptr, n);
+				alloc_.deallocate(ptr, n);
 			}
 			const size_type GetAllocMemSize() const {
 				return alloc_mem_size_;
@@ -412,9 +566,38 @@ namespace Shard
 				return !(lhs == rhs);
 			}
 		private:
-			Allocator&	alloc_;
+			Allocator<T>&	alloc_;
 			size_type	alloc_mem_size_{ 0u };
 			size_type	alloc_mem_count_{ 0u };
+		};
+
+		template<typename T, template<typename>typename Allocator>
+		class SynchronizedWrapAllocator : public Allocator<T>
+		{
+		public:
+			using value_type = T;
+			using Parent = Allocator<T>;
+			template<typename U>
+			struct rebind {
+				using other = SynchronizedWrapAllocator<U, Allocator>;
+			};
+		public:
+			[[nodiscard]] T* allocate(size_type n) {
+				SpinLock lock(lock_);
+				return Parent::allocate(n);
+			}
+			void deallocate(T* ptr, size_type n) {
+				SpinLock lock(lock_);
+				Parent::deallocate(ptr, n);
+			}
+			friend bool operator==(const SynchronizedWrapAllocator& lhs, const SynchronizedWrapAllocator& rhs) {
+				return (static_cast<const Parent&>(lhs)) == (static_cast<const Parent&>(rhs));
+			}
+			friend bool operator!=(const SynchronizedWrapAllocator& lhs, const SynchronizedWrapAllocator& rhs) {
+				return !(lhs == rhs);
+			}
+		private:
+			std::atomic_bool	lock_{ false };
 		};
 	}
 }
