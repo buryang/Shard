1c29adec5cd934b38fa0d74084d507174e2fae42
Buryang <419494197@163.com>
2024-01-11 09:55:41 +0800

    refactor job system code

--- Test/TestJobSystem.cpp
+++ Test/TestJobSystem.cpp
@@ -14,7 +14,7 @@ TEST(TEST_JOB_SYSTEM, TEST_CORO_TASK)
         std::cerr << "hello world! Coro" << std::endl;
         co_return;
     };
-    Schedule(hello_world);
+    Schedule(hello_world(), nullptr, 0xFFFFFFFF, false);
 
     auto counter_coro = []()->Coro<uint32_t>
     {
@@ -23,9 +23,8 @@ TEST(TEST_JOB_SYSTEM, TEST_CORO_TASK)
         {
             co_yield counter;
         }
-        co_return counter;
     };
-    Schedule(counter_coro);
+    Schedule(counter_coro(), nullptr, 0xFFFFFFFF, false);
 
     auto hello_world_number = [counter_coro]()->Coro<>
     {
@@ -37,35 +36,44 @@ TEST(TEST_JOB_SYSTEM, TEST_CORO_TASK)
         }
         co_return;
     };
-    Schedule(hello_world_number);
+    Schedule(hello_world_number(), nullptr, 0xFFFFFFFF, false);
 
     //test schedule seq
     {
+        std::atomic_bool flag{ false };
         auto counter = std::make_shared<uint32_t>(0u);
-        auto sequential_number = [counter]()->Coro<> {
+        auto sequential_number = [counter, &flag]()->Coro<> {
             auto inc_number = [counter]()->Coro<> {
                 (*counter)++;
                 co_return;
             };
-            co_await AwaitTupleHelper::Sequential(inc_number, inc_number, inc_number);
+            co_await AwaitTupleHelper::Sequential(inc_number(), inc_number(), inc_number());
+            flag.store(true, std::memory_order::release);
         };
-        auto seq_job = Schedule(sequential_number);
-        while (!seq_job->IsFinished()) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        Schedule(sequential_number());
+        while (!flag.load(std::memory_order::acquire)) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
-        ASSERT_EQ(*counter, 3);
+        EXPECT_EQ(*counter, 3);
     }
-
     //test schedule parallel
     {
-        auto parallel_number = []()->Coro<> {
-            auto inc_number = []()->Coro<> {
+        std::atomic_bool flag{ false };
+        std::atomic_int32_t counter{ 0u };
+        auto parallel_number = [&]()->Coro<> {
+            auto inc_number = [&counter]()->Coro<> {
                 std::cerr << "hello world! " << std::this_thread::get_id() << std::endl;
+                counter.fetch_add(1u, std::memory_order::acq_rel);
                 co_return;
             };
-            co_await AwaitTupleHelper::Parallel(inc_number, inc_number, inc_number);
+            co_await AwaitTupleHelper::Parallel(inc_number(), inc_number(), inc_number());
+            flag.store(true, std::memory_order::release);
         };
-        Schedule(parallel_number);
+        Schedule(parallel_number());
+        while (!flag.load(std::memory_order::acquire)) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+        EXPECT_EQ(counter.load(), 3);
     }
     
 }
@@ -73,30 +81,28 @@ TEST(TEST_JOB_SYSTEM, TEST_CORO_TASK)
 TEST(TEST_JOB_SYSTEM, TEST_FUNC_TASK)
 {
     auto hello_world = []() {
-        std::cerr << "hello world!, function";
+        std::cerr << "hello world!, function" << std::endl;
     };
-    auto hello_handle = Schedule(hello_world, nullptr, 0x1, false);
-    ASSERT_TRUE(hello_handle != nullptr);
+    std::atomic_bool flag{ false };
+    Schedule(hello_world, nullptr, 0xFFFFFFFFF, false);
 
-    auto cycle_print = []()->void {
+    auto cycle_print = [&flag]()->void {
         uint32_t counter = 0u;
         while (counter++ < 100u) {
             if (counter % 10 == 0) {
                 std::cerr << "cycle hello world! " << counter << std::endl;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(2));
         }
+        flag.store(true, std::memory_order::release);
     };
-    auto cycle_handle = Schedule(cycle_print);
-    ASSERT_TRUE(cycle_handle != nullptr);
+    Schedule(cycle_print, nullptr, 0xFFFFFFFF, true);
 
-    std::cerr << fmt::format("hello world task state: {}", hello_handle->IsFinished()) << std::endl;
-    std::cerr << fmt::format("cycle world task state: {}", cycle_handle->IsFinished()) << std::endl;
-    while (!hello_handle->IsFinished() || !cycle_handle->IsFinished()) {
+    while (!flag.load(std::memory_order::acquire))
+    {
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
-    std::cerr << fmt::format("hello world task state aft: {}", hello_handle->IsFinished()) << std::endl;
-    std::cerr << fmt::format("cycle world task state aft: {}", cycle_handle->IsFinished()) << std::endl;
+    EXPECT_EQ(flag.load(), true);
 }
 
 TEST(TEST_JOB_SYSTEM, TEST_MIX_TASK)
@@ -106,21 +112,28 @@ TEST(TEST_JOB_SYSTEM, TEST_MIX_TASK)
         std::cerr << "hello world! Coro" << std::endl;
         co_return;
     };
-    auto coro_handle = Schedule(hello_world_coro);
-    ASSERT_TRUE(coro_handle != nullptr);
+    
+    Schedule(hello_world_coro());
 
     auto hello_world_func = []() {
         std::cerr << "hello world!, function" << std::endl;
     };
-    auto func_handle = Schedule(hello_world_func, nullptr, 0x1, false);
-    ASSERT_TRUE(func_handle != nullptr);
+    
+    Schedule(hello_world_func, nullptr, 0x1, false);
+
+    auto mix_hello_world = [=]()->Coro<>{
+        co_await AwaitTupleHelper::Parallel(hello_world_func, hello_world_coro());
+    };
+    Schedule(mix_hello_world(), nullptr);
 
     //test continuation
-    auto hello_world_coro2 = []()->Coro<>
+    auto hello_world_func2 = [hello_world_coro]()
     {
         std::cerr << "hello world again! Coro" << std::endl;
-        co_return;
+        Continuation(hello_world_coro());
     };
+    Schedule(hello_world_func2, nullptr, 0x1, false);
+    SimpleJobSystem::Instance().UnInit();
 }
 
 #include "Utils/SimpleJobSystem.cpp"
